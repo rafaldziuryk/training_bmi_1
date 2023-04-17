1537a90048d5bbdfdbd778b50af1880a154cd3db
rafaldziuryk <rafal.dziuryk@appvinio.com>
2023-04-17 13:04:44 +0200

    add clickable button

--- lib/main.dart
+++ lib/main.dart
@@ -55,161 +55,176 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              "BMI Calculator",
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                "BMI Calculator",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Text("Gender"),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                      border: Border.all(
-                        color: Colors.greenAccent,
-                        width: 2,
+              SizedBox(
+                height: 40,
+              ),
+              Text("Gender"),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        border: Border.all(
+                          color: Colors.greenAccent,
+                          width: 2,
+                        ),
+                        color: Colors.white24,
                       ),
+                      height: 200,
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                              child: Icon(
+                            Icons.man,
+                            size: 150,
+                          )),
+                          Positioned(
+                              right: 8,
+                              top: 8,
+                              child: Icon(
+                                Icons.check_circle,
+                                color: Colors.green,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Container(
                       color: Colors.white24,
-                    ),
-                    height: 200,
-                    child: Stack(
-                      children: [
-                        Positioned.fill(
-                            child: Icon(
-                          Icons.man,
-                          size: 150,
-                        )),
-                        Positioned(
-                            right: 8,
-                            top: 8,
-                            child: Icon(
-                              Icons.check_circle,
-                              color: Colors.green,
-                            )),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.white24,
-                    height: 200,
-                    child: Stack(
-                      children: [
-                        Positioned.fill(
-                            child: Icon(
-                          Icons.woman,
-                          size: 150,
-                        )),
-                        Positioned(
-                            right: 8,
-                            top: 8,
-                            child: Icon(
-                              Icons.check_circle,
-                              color: Colors.grey,
-                            )),
-                      ],
+                      height: 200,
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                              child: Icon(
+                            Icons.woman,
+                            size: 150,
+                          )),
+                          Positioned(
+                              right: 8,
+                              top: 8,
+                              child: Icon(
+                                Icons.check_circle,
+                                color: Colors.grey,
+                              )),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Text("Weight"),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              children: <Widget>[
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                    height: 50,
-                    color: Colors.grey.shade100,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.remove_circle, color: Colors.black),
-                        SizedBox(
-                            width: 100,
-                            child: TextField(
-                              style: TextStyle(color: Colors.black),
-                            )),
-                        Icon(Icons.add_circle, color: Colors.black),
-                      ],
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text("Weight"),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      height: 50,
+                      color: Colors.grey.shade100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(Icons.remove_circle, color: Colors.black),
+                          SizedBox(
+                              width: 100,
+                              child: TextField(
+                                style: TextStyle(color: Colors.black),
+                              )),
+                          Icon(Icons.add_circle, color: Colors.black),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    height: 50,
-                    color: Colors.grey.shade100,
+                  SizedBox(
+                    width: 16,
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
-              children: <Widget>[
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                    height: 50,
-                    color: Colors.grey.shade100,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(Icons.remove_circle, color: Colors.black),
-                        SizedBox(
-                            width: 100,
-                            child: TextField(
-                              style: TextStyle(color: Colors.black),
-                            )),
-                        Icon(Icons.add_circle, color: Colors.black),
-                      ],
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      height: 50,
+                      color: Colors.grey.shade100,
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    height: 50,
-                    color: Colors.grey.shade100,
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      height: 50,
+                      color: Colors.grey.shade100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(Icons.remove_circle, color: Colors.black),
+                          SizedBox(
+                              width: 100,
+                              child: TextField(
+                                style: TextStyle(color: Colors.black),
+                              )),
+                          Icon(Icons.add_circle, color: Colors.black),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      height: 50,
+                      color: Colors.grey.shade100,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16,),
+              InkWell(
+                child: Container(
+                  height: 48,
+                  color: Colors.green,
+                  child: Center(
+                    child: Text("Click"),
                   ),
                 ),
-              ],
-            ),
-          ],
+                onTap:(){
+                  print("asdasd");
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
